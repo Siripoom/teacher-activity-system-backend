46994f207a2c4f07e0e6efb7627277b9be426298
Surachart01 <113654372+Surachart01@users.noreply.github.com>
2025-09-15 00:09:52 +0700

    add access filesActivity

--- src/controllers/fileControllers.js
+++ src/controllers/fileControllers.js
@@ -1,3 +1,6 @@
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 // Log helper
 const logAction = async (action, req, description) => {
     await prisma.log.create({
--- src/routes/fileRoutes.js
+++ src/routes/fileRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { uploadFile } from '../controllers/fileControllers.js';
+import { getFileById, uploadFile } from '../controllers/fileControllers.js';
 import multer from 'multer';
 const router = express.Router();
 
 //create log file
 const upload = multer({ dest: 'uploads/logStudent' });
-router.post('/upload-csv',upload.single('file') , uploadFile)
+router.post('/upload-csv', upload.single('file'), uploadFile);
+router.get('/activityfile/:id', getFileById);
 export default router;
